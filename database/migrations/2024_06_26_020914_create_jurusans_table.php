7c439d399b161c973566579f17296d47eb8213ef
Imam310402 <i.shidiq55@gmail.com>
2024-06-28 02:46:45 +0700

    Update 2024_06_26_020914_create_jurusans_table.php

--- database/migrations/2024_06_26_020914_create_jurusans_table.php
+++ database/migrations/2024_06_26_020914_create_jurusans_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('jurusan', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_jurusan');
+            $table->string('bank');
             $table->timestamps();
         });
     }
