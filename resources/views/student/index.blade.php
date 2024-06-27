16efbf05ef3047badc77f9305d981c84958601cb
Imam310402 <i.shidiq55@gmail.com>
2024-06-28 02:46:52 +0700

    Update index.blade.php

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,46 +1,68 @@
 @extends('template')
+
 @section('content')
 <main class="container">
     <div class="p-5 rounded">
-      <h1>Daftar Siswa</h1>
-      <a class="btn btn-danger" href="/student/create">Tambah Siswa Baru</a>
-      <hr>
-      @if(session('message')!='')
-      <div class="alert alert-primary" role="alert">
-        {{ session('message') }}
-      </div>
-      @endif
-      <table class="table table-bordered">
-        <tr>
-            <th>Nomor</th>
-            <th>NIS</th>
-            <th>Nama</th>
-            <th>Jurusan</th>
-            <th>Tanggal Lahir</th>
-            <th colspan="2" width="40">Action</th>
-        </tr>
-        @foreach($students as $student)
-        <tr>
-            <td>{{ $loop->iteration}}</td>
-            <td>{{ $student->nis}}</td>
-            <td>{{ $student->name}}</td>
-            <td>{{ $student->jurusan->nama_jurusan}}</td>
+        <h1>Tabungan Siswa SMK Muhammadiyah 15 Jakarta</h1>
+        <hr>
 
-            <td>{{ $student->birth_date}}</td>
-            <td>
-              <a href="/student/{{ $student->id }}/edit" class="btn btn-danger">Edit</a>
-          </td>
+        @if(session('message'))
+        <div class="alert alert-primary" role="alert">
+            {{ session('message') }}
+        </div>
+        @endif
 
-          <td>
-            <form action="/student/{{ $student->id }}" method="POST">
-              <input type="hidden" name="_method" value="DELETE">
-              <form action="/student/{{ $student->id }}">
-              @csrf
-              <button type="submit" class="btn btn-primary">Delete</button>
-              </form>
-           </td>
-        </tr>
-        @endforeach
-      </table>
+        <!-- Form Pencarian -->
+        <p>Masukan Nomor Rekening Jika Siswa Ingin Menabung</p>
+        <form action="{{ route('student.tabungan') }}" method="GET" class="mb-3">
+          
+          <div class="input-group">
+              
+              <input type="text" class="form-control" placeholder="Masukkan Nomor Rekening" name="norek" aria-label="Nomor Rekening" aria-describedby="button-addon2">
+              <button class="btn btn-primary" type="submit" id="button-addon2">Cari</button>
+          </div>
+      </form>
+      
+        <!-- Tabel Daftar Siswa -->
+        <table class="table table-bordered">
+            <thead>
+                <tr class="text-center">
+                    <th>No</th>
+                    <th>Nomor Rekening</th>
+                    <th>Nama</th>
+                    <th>Nama Bank</th>
+                    <th>EXP Card</th>
+                    <th>Total Tabungan</th>
+                    <th colspan="2" width="40">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($students as $student)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $student->norek }}</td>
+                    <td>{{ $student->name }}</td>
+                    <td>{{ $student->jurusan->bank }}</td>
+                    <td>{{ $student->expcard }}</td>
+                    <td>{{ $student->tab }}</td>
+                    <td>
+                        <a href="{{ route('student.edit', ['id' => $student->id]) }}" class="btn btn-danger">Edit</a>
+                    </td>
+                    <td>
+                        <form action="{{ route('student.destroy', ['id' => $student->id]) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit" class="btn btn-primary">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @empty
+                <tr>
+                    <td colspan="7" class="text-center">Tidak ada data siswa.</td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
     </div>
-  </main>
+</main>
+@endsection
