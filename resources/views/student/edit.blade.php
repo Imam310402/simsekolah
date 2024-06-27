106b06d1b6fc1932c7bbaafe31a11969a54c3af8
Imam310402 <i.shidiq55@gmail.com>
2024-06-28 02:46:49 +0700

    Update edit.blade.php

--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -1,35 +1,39 @@
 @extends('template')
 @section('content')
 <main class="container" style="margin-top: 30px">
-    <form action="/student/{{$student->id}}" method="PUT">
-        <input type="hidden" name="_method" value="PUT">
+    <form action="/student/{{ $student->id }}" method="POST">
+        @method('PUT')
         @csrf
-    <div class="p-5 rounded">
-      <h1>Tambah Siswa</h1>
-      <a class="btn btn-danger" href="/siswa/create">Tambah Siswa Baru</a>
-      <hr>
-      <table class="table table-bordered">
-        <tr>
-            <td>NIS</td>
-            <td><input type="text" value="{{$student->nis}}"name="nis" placeholder="NIS" class="form-control"></td>
-        </tr>
-        <tr>
-            <td>Nama</td>
-            <td><input type="text" value="{{$student->name}}" name="name" placeholder="nama" class="form-control"></td>
-        </tr>
-        <tr>
-            <td>Tanggal Lahir</td>
-            <td><input type="date" value="{{$student->birth_date}}" name="birth_date" placeholder="Tanggal Lahir" class="form-control"></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>
-                <button type="submit" class="btn btn-danger">Simpan</button>
-                <a class="btn btn-success" href="/student">Kembali</a>
-            </td>
-        </tr>
-      </table>
-    </div>
-</form>
-  </main>
-  @endsection
+        <div class="p-5 rounded">
+            <h1>Edit Tabungan Siswa</h1>
+            <hr>
+            <table class="table table-bordered">
+                <tr>
+                    <td>Nomor Rekening</td>
+                    <td><input type="text" value="{{ $student->norek }}" name="norek" placeholder="Nomor Rekening" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>Nama</td>
+                    <td><input type="text" value="{{ $student->name }}" name="name" placeholder="Nama" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>Nama Bank</td>
+                    <td><input type="text" value="{{ $student->jurusan->bank }}" name="bank" placeholder="Nama Bank" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>EXP Card</td>
+                    <td><input type="date" value="{{ $student->expcard }}" name="expcard" placeholder="EXP Card" class="form-control"></td>
+                </tr>
+              
+                <tr>
+                    <td></td>
+                    <td>
+                        <button type="submit" class="btn btn-danger">Simpan</button>
+                        <a class="btn btn-success" href="/student">Kembali</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</main>
+@endsection
